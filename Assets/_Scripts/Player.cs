dab403d1de6cd6f4b4a5f5bf2574b6f3c80c9892
Gabe Mertz <gabe.mertz@crms.org>
2019-04-13 15:39:33 -0700

    yeah man started up that player script and stuff

---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MovableObject
+{
+    // Start is called before the first frame update
+    public override void Start()
+    {
+		Init(gameObject.GetComponent<Rigidbody2D>());
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+		Vector2 inputDirection = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+		SetMoveDir(inputDirection.normalized);
+		SetLookPos(new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")));
+		MoveUpdate();
+    }
+}
